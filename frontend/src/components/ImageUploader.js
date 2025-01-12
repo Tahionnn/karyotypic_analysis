7dbdc23df029b58140f09a7d857f137f7e8b923c
Tahionnn <81324335+Tahionnn@users.noreply.github.com>
2025-01-12 18:05:20 +0400

    Visualization of predictions

--- frontend/src/components/ImageUploader.js
+++ frontend/src/components/ImageUploader.js
@@ -1,13 +1,161 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
+
+const BoxDrawer = ({ imageURL, prediction, originalShape }) => {
+    const canvasRef1 = useRef(null); // Для обычных боксов
+    const canvasRef2 = useRef(null); // Для заполненных боксов
+    const boxes = prediction.boxes;
+
+    const classColors = {
+        'A1': '255,0,0',   // красный
+        'A2': '0,0,255',   // синий
+        'A3': '0,128,0',   // зеленый
+        'B4': '255,255,0', // желтый
+        'B5': '128,0,128', // пурпурный
+        'C6': '255,165,0', // оранжевый
+        'C7': '0,255,255', // циан
+        'C8': '255,0,255', // магента
+        'C9': '0,255,0',   // лайм
+        'C10': '255,192,203', // розовый
+        'C11': '165,42,42', // коричневый
+        'C12': '128,128,128', // серый
+        'D13': '0,0,128',   // темно-синий
+        'D14': '0,128,128', // бирюзовый
+        'D15': '255,127,80', // коралловый
+        'E16': '250,128,114', // лососевый
+        'E17': '240,230,140', // хаки
+        'E18': '255,215,0', // золото
+        'F19': '238,130,238', // фиолетовый
+        'F20': '75,0,130', // индиго
+        'G21': '128,128,0', // оливковый
+        'G22': '230,230,250', // лаванда
+        'X': '0,191,255', // глубокий небесный синий
+        'Y': '255,105,180' // ярко-розовый
+    };
+
+    const drawBoxes = (canvas, imageUrl, boxes) => {
+        const context = canvas.getContext('2d');
+        const img = new Image();
+
+        img.onload = () => {
+            canvas.width = originalShape[1];
+            canvas.height = originalShape[0];
+            context.drawImage(img, 0, 0);
+
+            for (const classId in boxes) {
+                const boxArray = boxes[classId];
+                const color = classColors[classId];
+
+                for (const box of boxArray) {
+                    const [x1, y1, x2, y2] = box;
+
+                    context.strokeStyle = `rgba(${color}, 1)`;
+                    context.lineWidth = 2;
+                    context.strokeRect(x1, y1, x2 - x1, y2 - y1);
+
+                    context.fillStyle = `rgba(${color}, 1)`;
+                    context.font = '16px Arial';
+                    context.fillText(classId, x1, y1 - 5);
+                }
+            }
+        };
+        img.src = imageURL;
+    };
+
+    const drawFilledBoxes = (canvas, imageUrl, boxes) => {
+        const context = canvas.getContext('2d');
+        const img = new Image();
+
+        img.onload = () => {
+            canvas.width = originalShape[1];
+            canvas.height = originalShape[0];
+            context.drawImage(img, 0, 0);
+
+            for (const classId in boxes) {
+                const boxArray = boxes[classId];
+                const color = classColors[classId];
+
+                for (const box of boxArray) {
+                    const [x1, y1, x2, y2] = box;
+
+                    context.fillStyle = `rgba(${color}, 0.5)`;
+                    context.fillRect(x1, y1, x2 - x1, y2 - y1);
+
+                    context.fillStyle = `rgba(${color}, 1)`;
+                    context.font = '16px Arial';
+                    context.fillText(classId, x1, y1 - 5);
+                }
+            }
+        };
+        img.src = imageURL;
+    };
+
+    useEffect(() => {
+        const canvas1 = canvasRef1.current;
+        const canvas2 = canvasRef2.current;
+
+        if (canvas1 && canvas2 && Object.keys(boxes).length > 0) {
+            drawBoxes(canvas1, imageURL, boxes);
+            drawFilledBoxes(canvas2, imageURL, boxes);
+        }
+    }, [imageURL, prediction, originalShape]);
+
+    return (
+        <>
+            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                <canvas ref={canvasRef1} style={{ border: '1px solid black' }} />
+                <canvas ref={canvasRef2} style={{ border: '1px solid black' }} />
+            </div>
+            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                {Object.entries(boxes).flatMap(([classID, boxArray]) => 
+                    boxArray.map((box, index) => {
+                        const [x1, y1, x2, y2] = box;
+                        const width = x2 - x1;
+                        const height = y2 - y1;
+
+                        return (
+                            <>
+                            <figcaption>{classID}</figcaption>
+                            <div 
+                                key={`${classID}-${index}`}
+                                style={{
+                                    width: width,
+                                    height: height,
+                                    backgroundImage: `url(${imageURL})`,
+                                    backgroundPosition: `-${x1}px -${y1}px`,
+                                    backgroundSize: 'initial', 
+                                    border: '1px solid rgba(0, 0, 0, 0.5)',
+                                }}
+                            />
+                            </>
+                        );
+                    })
+                )}
+            </div>
+        </>
+    );
+};
+
+
 
 const ImageUploader = () => {
     const [selectedFile, setSelectedFile] = useState(null);
-    const [prediction, setPrediction] = useState(null);
-    const [classes, setClasses] = useState(Array().fill(null));
+    const [imageURL, setImageURL] = useState('');
+    const [prediction, setPrediction] = useState({ boxes: {}, classes: [] });
+    const [originalShape, setOriginalShape] = useState([0, 0]);
     
     const handleFile = (event) => {
-        setSelectedFile(event.target.files[0]);
+        const file = event.target.files[0];
+        setSelectedFile(file);
+
+        const img = new Image();
+        const url = URL.createObjectURL(file);
+        img.src = url;
+
+        img.onload = () => {
+            setOriginalShape([img.height, img.width]);
+            setImageURL(url);
+        };
     };
 
     const handleSubmit = async (event) => {
@@ -16,43 +164,39 @@ const ImageUploader = () => {
         formData.append('file', selectedFile);
 
         try {
-            const response = await axios.post('http://127.0.0.1:8000/predict', formData);/*, {
-                responseType: 'blob',
-            });*/
-            //const detection_imageURL = URL.createObjectURL(new Blob([response.data.detection_image.content]));
-            const detection_imageURL = `data:image/png;base64,${response.data.detection_image.content}`;
-            setPrediction(detection_imageURL)
-
-            /*const classes_imageURL = response.data.class_images.map( imageData => 
-                URL.createObjectURL(new Blob([imageData.content]))
-            );*/
-            const classes_imageURLs = response.data.class_images.map(imageData => 
-                `data:image/png;base64,${imageData.content}`
-            );
-            setClasses(classes_imageURLs);
-        }
-        catch (error) {
+            const response = await axios.post('http://127.0.0.1:8000/raw_prediction', formData);
+            const predictionData = {
+                boxes: response.data.boxes,
+                classes: response.data.classes,
+                confidences: response.data.confidences
+            };
+            setPrediction(predictionData);
+        } catch (error) {
             console.error('Error uploading image', error);
         }
     };
 
-    return(
+    return (
         <div>
             <h1>Upload your Chromosome ;)</h1>
             <form onSubmit={handleSubmit}>
-                <input type="file" onChange={handleFile} accept="image/*" required/>
+                <input type="file" onChange={handleFile} accept="image/*" required />
                 <button type='submit'>Submit</button>
             </form>
-            {prediction && <img src={prediction}></img>}
-            {(classes.length > 0) && (
-                <div>
-                    {classes.map((url, index) => (
-                        <img key={index} src={url} alt={`Class Image ${index}`}></img>
-                    ))}
-                </div>
+            {prediction.boxes && Object.keys(prediction.boxes).length > 0 && (
+                <>
+                <figure>
+                    <figcaption>Оригинальное изображение</figcaption>
+                    <img src={imageURL} alt="Uploaded" />
+                </figure>
+                <figure>
+                    <figcaption>Bounding Boxes</figcaption>
+                    <BoxDrawer imageURL={imageURL} prediction={prediction} originalShape={originalShape} />
+                </figure>
+                </>
             )}
         </div>
     );
-}
+};
 
 export default ImageUploader;
--- karyotypic_analysis/ml/router.py
+++ karyotypic_analysis/ml/router.py
@@ -3,6 +3,7 @@ from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from karyotypic_analysis.ml.YOLOv10 import YOLOv10
 from karyotypic_analysis.ml.models import *
+from karyotypic_analysis.ml.utils import rescale_boxes
 import numpy as np
 import cv2
 
@@ -21,17 +22,50 @@ app.add_middleware(
 model = YOLOv10(model_path="/home/tahion/Documents/GitHub/karyotypic_analysis/karyotypic_analysis/models/best.onnx") 
 model.load()
 
-@app.post("/predict")
-async def get_predictions(file: UploadFile = File(...)):
+
+@app.post("/raw_prediction")
+async def get_raw_predictions(file: UploadFile = File(...)):
     image = await file.read()
 
     np_array = np.frombuffer(image, np.uint8)
     
     image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
     
-    detection_buffer, class_images_buffer = model.postprocess(image)
+    image, original_shape = model.preprocess(image)
     
-    detection_image = ImageData.from_bytes(detection_buffer)
-    class_images = [ImageData.from_bytes(image) for image in class_images_buffer]
+    results = model.predict(image)
+    results = np.squeeze(results[0])
 
-    return Predictions(detection_image=detection_image, class_images=class_images)
+    boxes = results[:, :4]
+    confidences = results[:, 4]
+    classes = results[:, 5].astype(int)
+    height, width = original_shape[:2]
+    boxes = rescale_boxes(boxes=boxes, img_height=height, img_width=width)
+
+    class_dict = [
+        'A1', 'A2', 'A3', 'B4', 'B5',
+        'C6', 'C7', 'C8', 'C9', 'C10',
+        'C11', 'C12', 'D13', 'D14', 'D15',
+        'E16', 'E17', 'E18', 'F19', 'F20',
+        'G21', 'G22', 'X', 'Y'
+    ]
+
+    class_images = {class_name: [] for class_name in class_dict}
+
+    for i, box in enumerate(boxes):
+        x1, y1, x2, y2 = box
+        confidence = confidences[i]
+        class_id = classes[i]
+
+        if confidence > 0.5:
+            x1, y1, x2, y2 = map(float, [x1, y1, x2, y2])
+
+            class_id = int(class_id)
+            class_name = class_dict[class_id]
+            object_image = [x1, y1, x2, y2]
+
+            class_images[class_name].append(object_image)
+
+    return {
+        'boxes': class_images,  
+    }
